73189dd5fa652381f977ee364b1e67066a5745db
SpirkinArtyom <95916190+SpirkinArtyom@users.noreply.github.com>
2024-06-25 11:55:05 +0500

    Create LibrarySessionBean.java

---
+++ LibrarySessionBean.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class LibrarySessionBean {
+    @PersistenceContext(unitName = "LibraryPU")
+    private EntityManager em;
+
+    public List<Book> getAllBooks() {
+        return em.createQuery("SELECT b FROM Book b", Book.class).getResultList();
+    }
+
+    // Методы для добавления, редактирования и удаления книг
+}
